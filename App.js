3f745e3a7dee0119db1cfc4f1483376b349988b8
rits620 <ritikashukla620@gmail.com>
2024-08-11 21:32:03 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-// const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // console.log(heading);
-// root.render(heading);
+//root.render(heading);
 
 
 /**
@@ -24,9 +28,9 @@ const parent = React.createElement(
     React.createElement(
         "div",{id:"child"},
         [React.createElement(
-            "h1",{id:"head"},"I'm an h1 tag"
+            "h1",{id:"head"},"Namste React 🚀"
         ),React.createElement(
-            "h2",{id:"head"},"I'm an h2 tag"
+            "h2",{id:"head"},"Akshay Saini"
         )]
     )
 )
